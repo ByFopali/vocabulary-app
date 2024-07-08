e4e16532ab29355f7f852a9d7d3328572e9e8769
ByFopali <vadymboroliuk4@gmail.com>
2024-07-07 20:28:22 -0600

    edited models import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,16 +7,14 @@ __all__ = (
     "TopicWordAssociation",
     "Topic",
     "Language",
-    "LanguageUserAssociation",
     "User",
 )
 
 from .base import Base
-from .user import User
-from .word import Word
+from .db_helper import DatabaseHelper, db_helper
+from .grammar_element import GrammarElement
+from .language import Language
 from .topic import Topic
 from .topic_word_association import TopicWordAssociation
-from .language import Language
-from .language_user_association import LanguageUserAssociation
-from .grammar_element import GrammarElement
-from .db_helper import DatabaseHelper, db_helper
+from .user import User
+from .word import Word
